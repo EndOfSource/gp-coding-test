e49f97ffd8d8f83b0d95ef77037cb0ad81bece13
Dirk Cziesla <dirk_cziesla@hotmail.de>
2021-08-27 17:32:08 +0100

    use JSON datatype

--- create_schema.sql
+++ create_schema.sql
@@ -3,7 +3,7 @@ use greyparrot;
 CREATE TABLE `customer_addresses` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(191) DEFAULT NULL,
-  `address` text,
+  `address` JSON,
   `zip_code` varchar(8) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `zip_code` (`zip_code`)

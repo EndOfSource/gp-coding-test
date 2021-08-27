aa21444ed0276d6be12dbe0d63d284abd5778648
Dirk Cziesla <dirk_cziesla@hotmail.de>
2021-08-27 15:49:03 +0100

    Insert row

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,12 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
-router.post('/', function(req, res, next) {
+const getConnection = require('../database/mysql')
 
+router.post('/', function(req, res, next) {
   const { name, address, zip_code } = req.body
 
-  console.log(name, address, zip_code)
-  res.send(req.body)
+  getConnection(function(err, conn){
+    conn.query(
+      "INSERT INTO customer_addresses (name, address, zip_code) VALUES (?, ?, ?)", 
+    [name, JSON.stringify(address), zip_code], 
+    function(err, rows) {
+      console.log(err)
+      res.json(rows);
+    })
+  })
 });
 
 module.exports = router;

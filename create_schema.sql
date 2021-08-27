4e14249e7cf9a183cf5dfc3dc1e5fb3c37f05592
Dirk Cziesla <dirk_cziesla@hotmail.de>
2021-08-27 15:26:50 +0100

    create databse

---
+++ create_schema.sql
@@ -0,0 +1,10 @@
+use greyparrot;
+
+CREATE TABLE `customer_addresses` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(191) DEFAULT NULL,
+  `address` text,
+  `zip_code` varchar(8) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `zip_code` (`zip_code`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;

defd637770abedd7bbc9aa2317d40ce1739c4864
Dirk Cziesla <dirk_cziesla@hotmail.de>
2021-08-27 17:28:51 +0100

    no need for cookieParser, urlencoded or static files

--- api/app.js
+++ api/app.js
@@ -1,7 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var apiRouter = require('./routes/api');
@@ -10,9 +9,6 @@ var app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api', apiRouter);
 

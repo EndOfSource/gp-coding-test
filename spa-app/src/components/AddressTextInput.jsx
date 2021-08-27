768e9dbb1589685e5307ad2f0e26ade64e034d37
Dirk Cziesla <dirk_cziesla@hotmail.de>
2021-08-27 16:44:36 +0100

    add bootstrap styling

--- spa-app/src/components/AddressTextInput.jsx
+++ spa-app/src/components/AddressTextInput.jsx
@@ -3,9 +3,9 @@ const AddressTextInput = ({ name, label, value, onChange}) => {
   return (
     <label>
       {label}
-      <input name={`${name}_1`} value={value[`${name}_1`]} onChange={onChange} />
-      <input name={`${name}_2`} value={value[`${name}_2`]} onChange={onChange} />
-      <input name={`${name}_3`} value={value[`${name}_3`]} onChange={onChange} />
+      <input class="form-control" name={`${name}_1`} value={value[`${name}_1`]} onChange={onChange} />
+      <input class="form-control" name={`${name}_2`} value={value[`${name}_2`]} onChange={onChange} />
+      <input class="form-control" name={`${name}_3`} value={value[`${name}_3`]} onChange={onChange} />
     </label>    
   )
 }
--- spa-app/src/components/TextInput.jsx
+++ spa-app/src/components/TextInput.jsx
@@ -1,9 +1,9 @@
 
 const TextInput = ({ name, label, value, onChange}) => {
     return (
-        <label>
+        <label class="form-label">
         {label}:
-        <input name={name} value={value} onChange={onChange} />
+        <input class="form-control" name={name} value={value} onChange={onChange} />
       </label>
     )
 }

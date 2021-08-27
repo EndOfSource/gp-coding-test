3a366e11669ca167bed2c81a467c75a7eb1af053
Dirk Cziesla <dirk_cziesla@hotmail.de>
2021-08-27 16:52:20 +0100

    more margins

--- spa-app/src/components/AddressTextInput.jsx
+++ spa-app/src/components/AddressTextInput.jsx
@@ -4,9 +4,15 @@ const AddressTextInput = ({ name, label, value, onChange}) => {
     <div class="mb-3">
       <label>
         {label}
-        <input class="form-control" name={`${name}_1`} value={value[`${name}_1`]} onChange={onChange} />
-        <input class="form-control" name={`${name}_2`} value={value[`${name}_2`]} onChange={onChange} />
-        <input class="form-control" name={`${name}_3`} value={value[`${name}_3`]} onChange={onChange} />
+        <div class="mb-1">
+          <input class="form-control" name={`${name}_1`} value={value[`${name}_1`]} onChange={onChange} />
+        </div>
+        <div class="mb-1">
+          <input class="form-control" name={`${name}_2`} value={value[`${name}_2`]} onChange={onChange} />
+        </div>
+        <div class="mb-1">
+          <input class="form-control" name={`${name}_3`} value={value[`${name}_3`]} onChange={onChange} />
+        </div>
       </label>    
     </div>
   )

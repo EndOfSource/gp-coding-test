982d34bab28f1a71041c90591e16dc5f7ee7c42c
Dirk Cziesla <dirk_cziesla@hotmail.de>
2021-08-27 16:06:54 +0100

    Basic form

--- spa-app/src/App.js
+++ spa-app/src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class CustomerForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: null,
+      address_line_1: null,
+      address_line_2: null,
+      address_line_3: null,
+      zip_code: null,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit(event) {
+    alert('An essay was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input name="name" value={this.state.name} onChange={this.handleChange} />
+        </label>
+        <label>
+          Address:
+          <input name="address_line_1" value={this.state.address_line_1} onChange={this.handleChange} />
+          <input name="address_line_2" value={this.state.address_line_2} onChange={this.handleChange} />
+          <input name="address_line_3" value={this.state.address_line_3} onChange={this.handleChange} />
+        </label>
+        <label>
+          ZIP-Code:
+          <input name="zip_code" value={this.state.zip_code} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
 }
 
-export default App;
+export default CustomerForm
